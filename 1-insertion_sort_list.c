a366bba79793d8a5bf996cf1c7d098da6de36c7e
jfbm74 <jfbm74@gmail.com>
2020-06-14 14:42:48 -0500

    Adding task 2

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,28 +7,43 @@
 */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *pivot;
+	listint_t *head, *pivot;
 
-	if (list == NULL || *list == NULL)
-		return;
-	current = (*list)->next;
-	while (current != NULL)
+	head = pivot = *list;
+	while (pivot->next != NULL)
 	{
-		pivot = current->next;
-		while (current->prev != NULL && current->prev->n > current->n)
+		pivot = pivot->next;
+		if (head->n > pivot->n)
 		{
-			current->prev->next = current->next;
-			if (current->next != NULL)
-				current->next->prev = current->prev;
-			current->next = current->prev;
-			current->prev = current->next->prev;
-			current->next->prev = current;
-			if (current->prev == NULL)
-				*list = current;
-			else
-				current->prev->next = current;
-			print_list(*list);
+			while (pivot->n < head->n)
+			{
+				if (head->prev == NULL)
+				{
+					(pivot->next)->prev = head;
+					head->next = pivot->next, head->prev = pivot;
+					pivot->next = head, pivot->prev = NULL;
+					*list = pivot, pivot = head;
+				}
+				else if (pivot->next == NULL)
+				{
+					pivot->prev = head->prev;
+					pivot->next =  head, head->next = NULL;
+					head->prev = pivot, (pivot->prev)->next = pivot;
+					head = head->prev->prev;
+				}
+				else
+				{
+					(head->prev)->next = pivot;
+					(pivot->next)->prev = head;
+					head->next = pivot->next;
+					pivot->prev = head->prev;
+					pivot->next = head;
+					head->prev = pivot;
+					head = head->prev->prev;
+				}
+				print_list(*list);
+			}
 		}
-		current = pivot;
+		head = pivot;
 	}
 }
